d1d61ba7ab4fe7bf57b835db3f5b9f4f1d6dff05
Kishore V <kishore.v@exathought.com>
2026-08-09 23:32:09 +0530

    updated config

--- client/js/config.js
+++ client/js/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  websocketUrl: "https://barber-shop-rooms.onrender.com",
-  backendUrl: "https://barber-shop-rooms.onrender.com"
+  websocketUrl: "https://barber-shop-room.vercel.app/",
+  backendUrl: "https://barber-shop-room.vercel.app/"
 };
